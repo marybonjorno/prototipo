3bdf3e213c7c2b0e57dceba75bf439c5826e1077
marybonjorno <131000358+marybonjorno@users.noreply.github.com>
2024-04-05 12:32:46 -0300

    hdjg

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let temnpoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(temnpoFinal/1000);
-    let minutos = Math.floor(segundos/60);
+    let minutos = Math.floor(segundos/40);
     let horas = Math.floor(minutos/60);
     let dias = Math.floor(horas/24);
     
